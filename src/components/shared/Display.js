5583688e6ee1b4c04362bfd5fb28cb44e7f61399
mr.j237 <jfokoua@gmail.com>
2019-12-15 22:22:36 +0100

    :lipstick: Add outline

--- src/components/shared/Display.js
+++ src/components/shared/Display.js
@@ -8,7 +8,8 @@ const displayStyle = {
   justifyContent: "flex-end",
   alignItems: "flex-end",
   fontSize: "3em",
-  padding: "0 10px 10px 0"
+  padding: "0 10px 10px 0",
+  outline: "1px solid #888"
 };
 
 const Display = ({ input }) => {
